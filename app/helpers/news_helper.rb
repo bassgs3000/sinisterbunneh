ed1841fe807fb41d768226bf2263682d4b019616
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-02-19 10:05:33 -0600

    Worked on modifying the inline text editing

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,5 +1,5 @@
 module NewsHelper
   def convert(fulltext)
-    fulltext = fulltext.gsub(/\[url="(https?:\/\/[\S]+)"\s(\S+)\]/) {"<a href=\"#{$1}\">#{$2}</a>"}
+    fulltext = fulltext.gsub(/\<url="(https?:\/\/[\S]+)"\s(\S+)\s?\>/) {"<a href=\"#{$1}\">#{$2}</a>"}
   end
 end
