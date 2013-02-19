8b33b0776fd2b192eaa4384472b83b799068eea1
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-02-19 15:44:07 -0600

    Added the helper to the application helper, applied helper to posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png"
   end
+  def convert(fulltext)
+    fulltext = fulltext.gsub(/<url="(https?:\/\/[\S]+)">(.+)<\/url>/) {"<a href=\"#{$1}\">#{$2}</a>"}
+    fulltext = fulltext.gsub(/<url="(https?:\/\/[\S]+)">/) {"<a href=\"#{$1}\">#{$1}</a>"}
+    fulltext = fulltext.gsub(/<color="(\w+)">(.+)<\/color>/) {"<span style=\"color:#{$1};\">#{$2}</span>"}
+    fulltext = fulltext.gsub(/<img="(https?:\/\/[\S]+)">/) { "<img src=\"#{$1}\">" }
+    fulltext
+  end
 end
--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,7 +1,2 @@
 module NewsHelper
-  def convert(fulltext)
-    fulltext = fulltext.gsub(/<url="(https?:\/\/[\S]+)">(.+)<\/url>/) {"<a href=\"#{$1}\">#{$2}</a>"}
-    fulltext = fulltext.gsub(/<color="(\w+)">(.+)<\/color>/) {"<span style=\"color:#{$1};\">#{$2}</span>"}
-    fulltext
-  end
 end
