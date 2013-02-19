4bdeb4d626529becda6ac2f58f50dd1876550aac
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-02-19 15:21:45 -0600

    Changed the tags for url and color to use <>

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,5 +1,7 @@
 module NewsHelper
   def convert(fulltext)
-    fulltext = fulltext.gsub(/\<url="(https?:\/\/[\S]+)"\s(\S+)\s?\>/) {"<a href=\"#{$1}\">#{$2}</a>"}
+    fulltext = fulltext.gsub(/<url="(https?:\/\/[\S]+)">(.+)<\/url>/) {"<a href=\"#{$1}\">#{$2}</a>"}
+    fulltext = fulltext.gsub(/<color="(\w+)">(.+)<\/color>/) {"<span style=\"color:#{$1};\">#{$2}</span>"}
+    fulltext
   end
 end
