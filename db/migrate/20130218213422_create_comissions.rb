1003ba5002defb1297992c0e132a5115fb083243
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-02-20 08:11:19 -0600

    Fixed some vestiges of the incorrectly spelled commission

--- db/migrate/20130218213422_create_comissions.rb
+++
@@ -1,9 +0,0 @@
-class CreateComissions < ActiveRecord::Migration
-  def change
-    create_table :comissions do |t|
-      t.boolean :status
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130219215333_add_content_to_comissions.rb
+++
@@ -1,5 +0,0 @@
-class AddContentToComissions < ActiveRecord::Migration
-  def change
-    add_column :comissions, :content, :text
-  end
-end
