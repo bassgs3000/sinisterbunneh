73a0f9d10028d67f788cb338fe1ff53ce0fdacd3
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-02-21 19:48:04 -0600

    Updated the script to get rid of rvm

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require "bundler/capistrano"
 
 # RVM Capistrano Integration Code ##############################################
-set :rvm_ruby_string, 'ruby-1.9.3-p327'
+set :rvm_ruby_string, 'ruby-1.9.3-p327@global'
 set :rvm_type, :system
 set :rvm_install_pkgs, %w[libyaml openssl]
 set :rvm_install_ruby_params, '--with-opt-dir=/usr/local/rvm/usr'
@@ -10,13 +10,6 @@ before 'deploy:setup', 'rvm:install_rvm'
 before 'deploy:setup', 'rvm:install_ruby'
 before 'deploy:setup', 'rvm:install_pkgs'
 
-namespace :rvm do
-  task :trust_rvmrc do
-    run "rvm rvmrc trust #{release_path}"
-  end
-end
-
-after "deploy", "rvm:trust_rvmrc"
 ################################################################################
 require "rvm/capistrano"
 
@@ -41,7 +34,7 @@ set :rails_env, :production
 # Named Tasks ##################################################################
 namespace :db do
   task :db_config, :except => { :no_release => true }, :role => :app do
-    run "cp -f ~/database.yml #{release_path}/config/database.yml"
+    run "cp -f ~/DB/#{application}/database.yml #{release_path}/config/database.yml"
   end
 end
 
