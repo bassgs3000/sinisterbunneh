93ceee62f5227cf7da026f010d987357b21650a9
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-02-21 20:58:33 -0600

    tweaked file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,11 @@
 require "bundler/capistrano"
 
-# RVM Capistrano Integration Code ##############################################
-set :rvm_ruby_string, 'ruby-1.9.3-p327@global'
-set :rvm_type, :system
-set :rvm_install_pkgs, %w[libyaml openssl]
-set :rvm_install_ruby_params, '--with-opt-dir=/usr/local/rvm/usr'
-
-before 'deploy:setup', 'rvm:install_rvm'
-before 'deploy:setup', 'rvm:install_ruby'
-before 'deploy:setup', 'rvm:install_pkgs'
-
-################################################################################
-require "rvm/capistrano"
-
 # Git and Server Information ###################################################
+#default_run_options[:shell] = '/bin/bash'
+default_run_options[:pty] = false
 ssh_options[:forward_agent] = true
+
+set :ruby_version, "1.9.3-p327"
 set :application, "sinisterbunneh"
 set :scm, :git
 set :repository, "git@github.com:bassgs3000/sinisterbunneh.git"
@@ -33,17 +24,45 @@ set :rails_env, :production
 
 # Named Tasks ##################################################################
 namespace :db do
+  desc "Copies a prefilled database.yml to the current revision"
   task :db_config, :except => { :no_release => true }, :role => :app do
     run "cp -f ~/DB/#{application}/database.yml #{release_path}/config/database.yml"
   end
 end
 
+namespace :rbenv do
+  desc "Installs rbenv to server"
+  task :install do
+    run "git clone git://github.com/sstephenson/rbenv.git ~/.rbenv"
+    run "git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build"
+    run "echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.profile"
+    run "echo 'eval \"$(rbenv init -)\"' >> ~/.profile"
+    run "rbenv install #{ruby_version}"
+    run "rbenv rehash"
+  end
+
+  desc "Updates existing rbenv installation"
+  task :update do
+    run "cd ~/.rbenv && git pull"
+    run "rbenv rehash"
+  end
+
+  desc "Rehashes rbenv"
+  task :rehash do
+    run "rbenv rehash"
+  end
+end
+
 namespace :deploy do
+  desc "Creates the production database and precompiles assets"
   task :precompile, :role => :app do
     run "cd #{release_path}/ && RAILS_ENV=production rake db:create"
     run "cd #{release_path}/ && rake assets:precompile"
   end
 end
 
+#before "setup", "rbenv:install"
+#after "rbenv:install", "rbenv:rehash"
+#before "bundle:install", "rbenv:rehash"
 after "deploy", "db:db_config"
 after "deploy", "deploy:precompile"
